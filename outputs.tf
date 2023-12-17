58ca4fac52e1dd2b61221cbbe9884d08d3343ecb
Dave Mattice <david.mattice@hotmail.com>
2023-12-17 10:05:04 -0500

    cleanup

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,24 @@
 output "template" {
-  value = proxmox_virtual_environment_vm.template
+  description = "Full configuration of the crated template."
+  value       = proxmox_virtual_environment_vm.template
 }
 
 output "nodes" {
-  value = data.proxmox_virtual_environment_nodes.pve
+  description = "List of PVE nodes in the cluster pointed to by the PROXMOX Endpoint."
+  value       = data.proxmox_virtual_environment_nodes.pve
 }
 
 output "datastores" {
-  value = data.proxmox_virtual_environment_datastores.pve
+  description = "List of datastores found on the PVE node."
+  value       = data.proxmox_virtual_environment_datastores.pve
 }
 
 output "dns" {
-  value = data.proxmox_virtual_environment_dns.pve_node
+  description = "DNS configuration of the PVE node."
+  value       = data.proxmox_virtual_environment_dns.pve_node
 }
 
 output "image" {
-  value = proxmox_virtual_environment_file.image
+  description = "Details of the ISO image used to create the template."
+  value       = proxmox_virtual_environment_file.image
 }
